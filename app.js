5bd034e8e8e9b38dff22c8963c6c9541e0fd71d9
Raúl Jiménez <77923787+RaulJimenezAyza@users.noreply.github.com>
2022-11-05 18:55:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,154 +1,192 @@
-var DEBUG = false;
+let code = [];
+let remaining = [];
+const colors = ['red', 'yellow', 'green', 'blue', 'black'];
+let counter = 1;
+let counter2 = 1;
+let won = false;
 
-ko.components.register('mastermind-game', {
-  template: {
-    element: 'mastermind-game-template'
-  },
-  viewModel: MastermindComponent
+class Row {
+    constructor() {
+        this.turn = false;
+        this.spaces = [];
+    }
+}
+
+// create row instances
+const row1 = new Row();
+const row2 = new Row();
+const row3 = new Row();
+const row4 = new Row();
+const row5 = new Row();
+const row6 = new Row();
+const row7 = new Row();
+const row8 = new Row();
+
+
+//generate random colors
+for (i = 0; i < 4; i++) {
+    code.push(colors[Math.floor(Math.random() * 5)]);
+}
+
+//create copy of code
+let code1 = [...code];
+
+// show random colors
+function bgColor(e, c) {
+    $(`${e}`).css('background-color', `${c}`);
+}
+
+// check the row
+function checkRow(arr, row, next, prev) {
+    // check if answer === code
+    if (arr.join("") === code.join("")) {
+        won = true;
+        $('.code > span').slideDown();
+        $('.header').text("You've Cracked the Code").css('color', 'green');
+        for (i = 0; i < arr.length; i++) {
+            $(`.dots${row} > .one${i + 1}`).css('background-color', 'black');
+        }
+    } else {
+        // check for black pegs 
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === code[i]) {
+                $(`.dots${row} > .one${counter2}`).css('background-color', 'black');
+                code1.splice(i, 1, "");
+                remaining.push("empty");
+                counter2 += 1;
+            } else {
+                remaining.push(arr[i]);
+            }
+        }
+        //check for white pegs
+        for (i = 0; i < arr.length; i++) {
+            if (code1.includes(remaining[i])) {
+                code1.splice(code1.indexOf(remaining[i]), 1, "blank");
+                $(`.dots${row} > .one${counter2}`).css('background-color', 'white');
+                counter2 += 1;
+            }
+        }
+        //reset to defaults + switch turns
+        counter = 1;
+        counter2 = 1;
+        next.turn = true;
+        prev.turn = false;
+        code1 = [...code];
+        remaining = [];
+    }
+}
+
+// add background color of code to the divs
+bgColor('.color-one', `${code[0]}`);
+bgColor('.color-two', `${code[1]}`);
+bgColor('.color-three', `${code[2]}`);
+bgColor('.color-four', `${code[3]}`);
+
+//gameplay
+
+//start game
+row8.turn = true;
+
+//add picked colors to board and appropriate arrays
+$('.bottom > span').on('click', (e) => {
+    let clickedItem = e.target;
+    if (clickedItem.className !== 'delete' && clickedItem.className !== 'submit') {
+        if (row8.turn && row8.spaces.length < 4) {
+            row8.spaces.push(clickedItem.className);
+            $(`.eight-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row7.turn && row7.spaces.length < 4) {
+            row7.spaces.push(clickedItem.className);
+            $(`.seven-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row6.turn && row6.spaces.length < 4) {
+            row6.spaces.push(clickedItem.className);
+            $(`.six-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row5.turn && row5.spaces.length < 4) {
+            row5.spaces.push(clickedItem.className);
+            $(`.five-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row4.turn && row4.spaces.length < 4) {
+            row4.spaces.push(clickedItem.className);
+            $(`.four-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row3.turn && row3.spaces.length < 4) {
+            row3.spaces.push(clickedItem.className);
+            $(`.three-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row2.turn && row2.spaces.length < 4) {
+            row2.spaces.push(clickedItem.className);
+            $(`.two-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        } else if (row1.turn && row1.spaces.length < 4) {
+            row1.spaces.push(clickedItem.className);
+            $(`.one-${counter}`).css('background-color', `${clickedItem.className}`);
+            counter += 1;
+        }
+    }
 });
 
-function MastermindComponent(params) { 
-  var self = this,
-    params = _.extend({
-      colors: 6, // difference colors to use
-      rows: 10,
-      controlObservable: null, // an observable passed from the main viewmodes which will control starting/pausing/stopping the game
-      win: 4
-    }, params || {}),
-    colors = ['red', 'blue', 'white', 'green', 'black', 'yellow'],
-
-    utils = {
-      randomColor: function() {
-        return colors[Math.floor(Math.random() * colors.length)];
-      },
-      populateWinningSequence: function() { // generate a new group of colors to be matched
-        self.winningSequence(self.winRange.map(utils.randomColor));
-      },
-      resetGame: function() {
-        self.playing(false);
-        self.revealing(false);
-        self.selectedIndex(0);
-        self.winningSequence.removeAll();
-        self.historySequence.removeAll();
-        self.playingSequence.removeAll();
-        self.pegSequence.removeAll();
-      },
-      testForMatch: function() {
-        var q = self.winningSequence().slice(),
-          rq = self.playingSequence(),
-          testColor = function(color, i) {
-            return color === q[i];
-          },
-          matches = rq.filter(testColor),
-          misses = _.reject(rq, testColor),
-          misplaced = [];
-        matches.forEach(function(c, i) {
-          q.splice(q.indexOf(c), 1);
-        });
-        misses.forEach(function(c) {
-          var k = q.indexOf(c);
-          if (k >= 0) {
-            q.splice(k, 1);
-            misplaced.push(c);
-          }
-        });
-
-        return {
-          correct: matches.length,
-          misplaced: misplaced.length
-        };
-
-      }
-    };
-
-  _.extend(self, { // properties and methods available to the template
-    DEBUG: ko.observable(DEBUG),
-    revealing: ko.observable(false), // reveal the winning combo
-    playing: ko.observable(false),
-    selectedIndex: ko.observable(0),// the selected marble crater which will apply a clicked color
-    rowRange: _.range(params.rows).reverse(),// [9,8,7,..,0]
-    winRange: _.range(params.win),// [0,1,2,3]
-    winningSequence: ko.observableArray(),
-    playingSequence: ko.observableArray(),
-    pegSequence: ko.observableArray(),
-    historySequence: ko.observableArray(),
-    colors: colors,
-    startGame: function() {
-      utils.resetGame();
-      utils.populateWinningSequence();
-      self.playing(true);
-    },
-    endGame: function( isWinner ) {
-      self.playing(false);
-      self.revealing(true);
-      if( isWinner ){
-      }
-    },
-    getPegClass: function(row, index) {
-      var matches = self.pegSequence()[params.rows - row - 1];
-      if (matches && matches.hasOwnProperty('correct')) {
-        return index < matches.correct ? 'correct' : index < matches.correct + matches.misplaced ? 'misplaced' : '';
-      }
-    },
-    setColor: function(color) { // a colored marble was clicked
-      var index = self.selectedIndex(),
-        rq = self.playingSequence();
-      rq[index] = color;
-      self.playingSequence(rq);
-     // if (_.reject(rq, _.isUndefined).length === params.win) { // full tray
-     // } else {
-      if( rq.length < params.win ){
-        var i = rq.indexOf(undefined);
-        i = i >= 0 ? i : rq.length;
-        self.selectedIndex(i);
-      }
-    },
-    submitRow: function() {
-      var matches = utils.testForMatch();
-      self.historySequence.push(self.playingSequence.slice());
-      self.pegSequence.push(matches);
-      self.playingSequence.removeAll();
-      //console.log('matches', matches);
-      if (matches.correct === params.win || self.historySequence().length >= params.rows) {
-        self.endGame(matches.correct === params.win);
-      } else {
-        self.selectedIndex(0);
-      }
+// remove color from board and appropriate arrays
+$('.delete').on('click', () => {
+    if (won === false) {
+        if (row8.turn && counter > 1) {
+            row8.spaces.pop();
+            counter -= 1;
+            $(`.eight-${counter}`).css('background-color', '#CECECE');
+        } else if (row7.turn && counter > 1) {
+            row7.spaces.pop();
+            counter -= 1;
+            $(`.seven-${counter}`).css('background-color', '#CECECE');
+        } else if (row6.turn && counter > 1) {
+            row6.spaces.pop();
+            counter -= 1;
+            $(`.six-${counter}`).css('background-color', '#CECECE');
+        } else if (row5.turn && counter > 1) {
+            row5.spaces.pop();
+            counter -= 1;
+            $(`.five-${counter}`).css('background-color', '#CECECE');
+        } else if (row4.turn && counter > 1) {
+            row4.spaces.pop();
+            counter -= 1;
+            $(`.four-${counter}`).css('background-color', '#CECECE');
+        } else if (row3.turn && counter > 1) {
+            row3.spaces.pop();
+            counter -= 1;
+            $(`.three-${counter}`).css('background-color', '#CECECE');
+        } else if (row2.turn && counter > 1) {
+            row2.spaces.pop();
+            counter -= 1;
+            $(`.two-${counter}`).css('background-color', '#CECECE');
+        } else if (row1.turn && counter > 1) {
+            row1.spaces.pop();
+            counter -= 1;
+            $(`.one-${counter}`).css('background-color', '#CECECE');
+        }
     }
-  });
-  //computed observables
-  self.rowMaster = ko.pureComputed(function() {
-    var pq = self.historySequence(),
-      nil = function() {
-        return null;
-      };
-    return self.rowRange.map(function(i) {
-      var ret = {
-        current: i === pq.length,
-        colors: []
-      };
-      if (i < pq.length) {
-        ret.colors = pq[i];
-      } else if (pq.length === i) {
-        ret.colors = self.winRange.map(function(k) {
-          return self.playingSequence()[k] || null;
-        });
-      } else {
-        ret.colors = self.winRange.map(nil);
-      }
-      return ret;
-    });
-  });
-  self.fullPlayerTray = ko.pureComputed(function() {
-    return self.playingSequence().length === params.win;
-  });
-}
+});
 
-function VM() {
-  var self = this;
-  self.mastermindControl = ko.observable('stop');
-}
-
-window.onload = function() {
-  ko.applyBindings(new VM());
-}
+// initate check function to compare row to codes
+$('.submit').on('click', () => {
+    if (row8.turn && row8.spaces.length === 4) {
+        checkRow(row8.spaces, 8, row7, row8);
+    } else if (row7.turn && row7.spaces.length === 4) {
+        checkRow(row7.spaces, 7, row6, row7);
+    } else if (row6.turn && row6.spaces.length === 4) {
+        checkRow(row6.spaces, 6, row5, row6);
+    } else if (row5.turn && row5.spaces.length === 4) {
+        checkRow(row5.spaces, 5, row4, row5);
+    } else if (row4.turn && row4.spaces.length === 4) {
+        checkRow(row4.spaces, 4, row3, row4);
+    } else if (row3.turn && row3.spaces.length === 4) {
+        checkRow(row3.spaces, 3, row2, row3);
+    } else if (row2.turn && row2.spaces.length === 4) {
+        checkRow(row2.spaces, 2, row1, row2);
+    } else if (row1.turn && row1.spaces.length === 4) {
+        checkRow(row1.spaces, 1, "", row1);
+        if (won === false) {
+            $('.code > span').slideDown();
+            $('.header').text("Failed").css('color', 'red');
+        }
+    }
+});
